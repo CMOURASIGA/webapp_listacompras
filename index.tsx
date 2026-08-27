f0c855d87102092835a796a7d865755cfc5e0794
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2026-08-27 18:26:53 -0300

    refactor: remove legacy settings providers from local-first app

--- index.tsx
+++ index.tsx
@@ -1,22 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AppSettingsProvider } from './store/appSettingsStore';
-import { NotificationsProvider } from './store/notificationsStore';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <AppSettingsProvider>
-      <NotificationsProvider>
-        <App />
-      </NotificationsProvider>
-    </AppSettingsProvider>
+    <App />
   </React.StrictMode>
 );
